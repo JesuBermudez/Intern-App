1433d842c1678b1acf5d77f248c9a912da3fe14e
Jesus Bermudez <jesmanuelber@gmail.com>
2024-11-02 15:39:53 -0500

    .

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,5 @@
 import React from "react";
 
-const Home = () => {
-  return <div className="">home</div>;
-};
-
-export default Home;
+export default function Home() {
+  return <section></section>;
+}
