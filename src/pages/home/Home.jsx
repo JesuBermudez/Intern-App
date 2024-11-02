88a1a6464046b3c6f29a726444a1f5b72ebba8b6
Jesus Bermudez <jesmanuelber@gmail.com>
2024-11-02 15:58:54 -0500

    Assets

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,5 +1,11 @@
-import React from "react";
+import logo from "../../assets/logo.svg";
 
 export default function Home() {
-  return <section></section>;
+  return (
+    <div className="bg-accent w-full h-screen flex justify-center">
+      <section className="">
+        <img src={logo} alt="Intern Logo" className="w-12" />
+      </section>
+    </div>
+  );
 }
