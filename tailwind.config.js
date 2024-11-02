e45898df6bd1d23d7fce8d739129a407e5fa3c3a
Jesus Bermudez <jesmanuelber@gmail.com>
2024-11-02 15:44:06 -0500

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#53FC18",
+        secondary: "#D9D9D9",
+        accent: "#161616",
+      },
+    },
   },
   plugins: [],
-}
-
+};
