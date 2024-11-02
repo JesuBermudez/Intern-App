1bf47c55ff7fa5a97cbc188f6c459baddea061fa
Jesus Bermudez <jesmanuelber@gmail.com>
2024-11-02 16:40:10 -0500

    .

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,10 @@
-import React from "react";
-
+/* eslint-disable react/prop-types */
 const Button = ({ params }) => {
-  return <button onClick={params.onClick} className={params.styles}>{params.text}</button>;
+  return (
+    <button onClick={params.onClick} className={params.styles}>
+      {params.text}
+    </button>
+  );
 };
 
 export default Button;
