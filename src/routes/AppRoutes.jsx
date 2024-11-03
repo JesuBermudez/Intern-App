01c1b782e814908f523ff484d93848c15f610971
Jesus Bermudez <jesmanuelber@gmail.com>
2024-11-02 20:16:21 -0500

    cambios variados

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "../pages/home/Home";
 import Register from "../pages/auth/Auth";
+import Main from "../pages/main/Main";
 const AppRoutes = () => {
   return (
     <BrowserRouter>
@@ -8,6 +9,7 @@ const AppRoutes = () => {
         <Route path="/" element={<Home />}></Route>
         <Route path="/register" element={<Register type="register" />}></Route>
         <Route path="/login" element={<Register type="login" />}></Route>
+        <Route path="/main" element={<Main></Main>}></Route>
       </Routes>
     </BrowserRouter>
   );
