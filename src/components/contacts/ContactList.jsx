a17cbde9a5005071d8a0a63b55def0dc59e4e783
leowader <waderhe@gmail.com>
2024-11-03 20:44:31 -0500

    chats ui ok

--- src/components/contacts/ContactList.jsx
+++ src/components/contacts/ContactList.jsx
@@ -1,20 +1,20 @@
-import userSvg from "../../assets/user-svgrepo-com.svg";
+import userSvg from "../../assets/user.svg";
 
 export default function ContactList({ contacts = [] }) {
   console.log(contacts);
 
   return (
-    <section className="h-1/2 w-full lg:w-1/3 p-2 overflow-y-scroll scrollbar-hide">
+    <section className="h-1/2 w-full lg:w-1/3 p-2 overflow-y-scroll scrollbar-hide mb-5">
       {contacts.map((contact) => (
         <article
           key={contact.contact_user_id}
-          className="group p-2 mb-2 border rounded-lg hover:rounded-2xl hover:bg-secondary/20 flex gap-3 cursor-pointer"
+          className=" items-center group p-2 mb-2 border border-white border-opacity-20 rounded-lg  bg-opacity-5 hover:rounded-2xl hover:bg-secondary/20 flex gap-3 cursor-pointer"
         >
-          <div className="rounded-2xl overflow-hidden p-[8px] border size-16">
-            <img src={userSvg} alt="User Profile" className="rounded-lg" />
+          <div className="flex border border-primary border-opacity-20 bg-accent items-center justify-center rounded-full overflow-hidden   size-10">
+          <img src={userSvg} alt="" />
           </div>
           <div className="flex flex-col justify-center">
-            <h3 className="text-xl font-medium">{contact.username}</h3>
+            <h3 className=" font-medium">{contact.username}</h3>
             <p className="text-slate-50/65">escribiendo...</p>
           </div>
         </article>
